370f6dadb3a8a8c3f2432c94c0acf0e75376c0df
Jacques Lucke <mail@jlucke.com>
2022-04-18 10:02:38 +0200

    progress

--- playground.py
+++ playground.py
@@ -23,29 +23,29 @@ blend_script_path = "/home/jacques/Documents/nobreak/playground_blender.py"
 
 with NobreakServer(nobreak_binary_path) as server:
 
-    subprocess.run(
-        [
-            blender_path,
-            blend_file_path,
-            "--python",
-            blend_script_path,
-            "-b",
-        ],
-        env={
-            "NOBREAK_PYTHON_INCLUDE": "/home/jacques/Documents/nobreak/clients/python",
-            "NOBREAK_API_URL": server.api_url,
-            "NOBREAK_MODE": "CHECK",
-        },
-    )
+    # subprocess.run(
+    #     [
+    #         blender_path,
+    #         blend_file_path,
+    #         "--python",
+    #         blend_script_path,
+    #         "-b",
+    #     ],
+    #     env={
+    #         "NOBREAK_PYTHON_INCLUDE": "/home/jacques/Documents/nobreak/clients/python",
+    #         "NOBREAK_API_URL": server.api_url,
+    #         "NOBREAK_MODE": "CHECK",
+    #     },
+    # )
 
-    # client = nobreak.Client(server.api_url, "UPDATE")
-    # tester = nobreak.Tester(client)
-    # tester.test("QWE", 5)
-    # tester.test("lala", -6)
-    # tester.test("fas", b"my_bytes")
-    # tester.test("vxc", 4.1)
+    client = nobreak.Client(server.api_url, "UPDATE")
+    tester = nobreak.Tester(client)
+    tester.test("QWE", 5)
+    tester.test("lala", -6)
+    tester.test("fas", b"my_bytes")
+    tester.test("vxc", 4.1)
 
-    # if sub_tester := tester.sub("D"):
-    #     sub_tester.test("E", "qwe")
+    if sub_tester := tester.sub("D"):
+        sub_tester.test("E", "qwe")
 
     print(server.load_log())
